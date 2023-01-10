0e72eaba516ca9f44f700ad191cfaf1a94c81954
danish khan <dkhan@deqode.com>
2023-01-10 18:52:37 +0530

    common component

--- iOS-trader/Screen/ViewController.swift
+++ iOS-trader/Screen/ViewController.swift
@@ -8,20 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameTextField : customUITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let nib = AppTextField()
+        self.view.addSubview(nib)
         addCom(self)
         // Do any additional setup after loading the view.
     }
-    
+    @IBAction func loginAction(_ sender: UIButton){
+        if nameTextField.text == ""{
+        nameTextField.setup()
+        }
+    }
     func addCom(_ view: UIViewController){
         let logo = LogoViewController(nibName: "LogoViewController", bundle: nil)
-        
         view.view.center = logo.view.center
-        
         view.view.addSubview(logo.view)
-        
+
+//        let textField = AppTextFiled(nibName: "AppTextFiled", bundle: nil)
+//        view.view.center = textField.view.center
+//        view.view.addSubview(textField.view)
+//
         
     }
 
